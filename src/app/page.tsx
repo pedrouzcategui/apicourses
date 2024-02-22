0e1795caf0cfa6a40b60f22f88b6da0f42015788
Pedro <pedrouzcategui0103@gmail.com>
2024-02-21 23:31:54 -0400

    Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,9 @@ import { YouTubeVideoPlayer } from "@/components";
 
 export default function Home() {
   return (
-    <>
-
-    </>
+    <div>
+      <h1>Learn Automations</h1>
+      <p>With these courses</p>
+    </div>
   );
 }
